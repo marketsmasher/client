ea9cbf6639ae56d1cd537cc09b70d790d9128a63
DancingCerberus <edikchermenin@gmail.com>
2025-04-09 16:07:20 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# client
+
+Marketsmasher Flutter app
---
+++ android/app/src/main/kotlin/com/marketsmasher/client/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.marketsmasher.client
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
